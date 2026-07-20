a165855291545ab613ca6f2f3c0c981f9ed9beb3
NatanaelDev <natanaelpereira745@gmail.com>
2026-07-20 11:46:32 -0300

    fix/Atividade finalizada Sistema Eventos

---
+++ src/main/java/com/SistemaEvento/SistemaEvento/SistemaEventoApplication.java
@@ -0,0 +1,13 @@
+package com.SistemaEvento.SistemaEvento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEventoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/SistemaEvento/SistemaEvento/entities/Atividade.java
@@ -0,0 +1,96 @@
+package com.SistemaEvento.SistemaEvento.entities;
+
+import jakarta.persistence.*;
+
+import java.util.*;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+
+    @OneToMany(mappedBy = "atividade", cascade = CascadeType.ALL)
+    private List<Bloco> blocos = new ArrayList<>();
+
+
+    @ManyToMany(mappedBy = "atividades")
+    private Set<Participante> participantes = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    public Atividade() {
+    }
+
+    public Atividade(Integer id, String nome, String descricao, Double preco, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public Double getPreco() {
+        return this.preco;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Atividade atividade = (Atividade) o;
+        return Objects.equals(id, atividade.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
